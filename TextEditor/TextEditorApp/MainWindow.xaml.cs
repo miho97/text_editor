97b13823533a0680e02f52009d20de1d724a9b49
CrnkovicMatej <matejj.crnkovic@gmail.com>
2023-12-19 02:27:25 +0100

    Text_editor initial commit - basic functionality and gui implemented; everything needs to be refactored

---
+++ TextEditor/TextEditorApp/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextEditorApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public  int fileCount = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void NewFile_Click(object sender, RoutedEventArgs e)
+        {
+
+            TabItem newTab = new TabItem();
+            newTab.Header = "File " + (++fileCount);
+
+            DockPanel panel = new DockPanel();
+
+            TextBlock statusBar = new TextBlock();
+            statusBar.Text = "Status bar for " + newTab.Header;
+            DockPanel.SetDock(statusBar, Dock.Bottom);
+
+            TextBox textBox = new TextBox();
+            textBox.VerticalScrollBarVisibility = ScrollBarVisibility.Visible;
+            DockPanel.SetDock(textBox, Dock.Top);
+
+            panel.Children.Add(statusBar);
+            panel.Children.Add(textBox);
+
+            newTab.Content = panel;
+
+            MainTabControl.Items.Add(newTab);
+
+            MainTabControl.SelectedItem = newTab;
+        }
+    }
+}
