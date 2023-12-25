74b0e55d2b1c6c8a0f3f79b423895e9a476617b7
miho97 <mihovil.strucic@gmail.com>
2023-12-25 15:54:12 +0100

    changing font size enabled

---
+++ TextEditor/inputDialog.xaml.cs
@@ -0,0 +1,22 @@
+
+
+
+namespace TextEditorApp
+{
+	/// <summary>
+	/// Interaction logic for inputDialog.xaml
+	/// </summary>
+	public partial class inputDialog : Window
+	{	
+		public inputDialog()
+		{
+			initializeComponent();
+		}
+
+		private void OK_button_click(object sender, RoutedEventsArgs e)
+		{
+
+		}
+	}
+}
+
