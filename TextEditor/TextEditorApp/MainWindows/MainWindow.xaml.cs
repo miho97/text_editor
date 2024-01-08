409bf35f5537f5b4c2db451248dd279cac2b0dd2
CrnkovicMatej <matejj.crnkovic@gmail.com>
2024-01-09 00:26:47 +0100

    MainWindow.xaml.cs clean-up Handler for alignment added to MainView Additional bindings C# highlight ribbonmenu removed

--- TextEditor/TextEditorApp/MainWindows/MainWindow.xaml.cs
+++ TextEditor/TextEditorApp/MainWindows/MainWindow.xaml.cs
@@ -32,6 +32,7 @@ using System.Runtime.ConstrainedExecution;
 using CefSharp;
 using CefSharp.Wpf;
 using TextEditorApp.MainWindows.Behaviours;
+using System.Globalization;
 
 namespace TextEditorApp
 {
@@ -46,115 +47,5 @@ namespace TextEditorApp
 
             DataContext = new MainWinViewModel(MainTabControl, FontSizeComboBox);
         }
-
-        private async void OnMainWindowLoaded(object sender, RoutedEventArgs e)
-        {
-            // Pristup StackPanel-u unutar ScrollViewer-a
-            if (sender is RoslynCodeEditor editor)
-            {
-                // Pristup RoslynCodeEditor-u unutar StackPanel-a
-                var roslynCodeEditor = editor as RoslynCodeEditor;
-
-                if (roslynCodeEditor != null)
-                {
-                    //await InitializeRoslynCodeEditorAsync(roslynCodeEditor);
-                }
-            }
-        }
-
-        private void OnLanguageChanged(object sender, RoutedEventArgs e)
-        {
-            if (sender is ComboBox languageCombobox)
-            {
-                var SelectedLanguage = languageCombobox.SelectedItem as string;
-                // Pristup RoslynCodeEditor-u unutar StackPanel-a
-                if (MainTabControl.SelectedItem is TabItem selectedTab && selectedTab.Content is DockPanel dockPanel)
-                {
-                    var textEditor = dockPanel.Children.OfType<RoslynCodeEditor>().FirstOrDefault();
-                    if (textEditor != null)
-                    {
-                        textEditor.SyntaxHighlighting =  HighlightingManager.Instance.GetDefinition(SelectedLanguage);
-                    }
-                }
-            }
-
-        }
-
-        private async Task InitializeRoslynCodeEditorAsync(RoslynCodeEditor roslynCodeEditor)
-        {
-            var roslynHost = new RoslynHost(additionalAssemblies: new[]
-             {
-            Assembly.Load("RoslynPad.Roslyn.Windows"),
-            Assembly.Load("RoslynPad.Editor.Windows"),
-        });
-            var _host = new CustomRoslynHost(additionalAssemblies: new[]
-        {
-                    Assembly.Load("RoslynPad.Roslyn.Windows"),
-                    Assembly.Load("RoslynPad.Editor.Windows")
-                }, RoslynHostReferences.NamespaceDefault.With(assemblyReferences: new[]
-        {
-            typeof(object).Assembly,
-            typeof(System.Text.RegularExpressions.Regex).Assembly,
-            typeof(Enumerable).Assembly,
-        }));
-
-            roslynCodeEditor.ShowLineNumbers = true;
-
-            var workingDirectory = Directory.GetCurrentDirectory();
-            var documentId = await roslynCodeEditor.InitializeAsync(_host, new ClassificationHighlightColors(),
-            workingDirectory, string.Empty, SourceCodeKind.Script).ConfigureAwait(true);
-            //await roslynCodeEditor.InitializeAsync(roslynHost, new ClassificationHighlightColors(), workingDirectory, "", SourceCodeKind.Script);
-        }
-
-        private async void Intellisese_Handler(object sender, RoutedEventArgs e)
-        {
-            if (sender is RibbonToggleButton toggleButton)
-            {
-                if (toggleButton.IsChecked == true)
-                {
-                    if (MainTabControl.SelectedItem is TabItem selectedTab && selectedTab.Content is DockPanel dockPanel)
-                    {
-                        var textEditor = dockPanel.Children.OfType<RoslynCodeEditor>().FirstOrDefault();
-                        if (textEditor != null)
-                        {
-                            await InitializeRoslynCodeEditorAsync(textEditor);
-                        }
-                    }
-                }
-                else
-                {
-                    if (MainTabControl.SelectedItem is TabItem selectedTab && selectedTab.Content is DockPanel dockPanel)
-                    {
-                        var textEditor = dockPanel.Children.OfType<RoslynCodeEditor>().FirstOrDefault();
-                        if (textEditor != null)
-                        {
-                            textEditor.CompletionProvider = null;
-                        }
-                    }
-                }
-            }
-        }
-
-        private class CustomRoslynHost : RoslynHost
-        {
-            private bool _addedAnalyzers;
-
-            public CustomRoslynHost(IEnumerable<Assembly>? additionalAssemblies = null, RoslynHostReferences? references = null, ImmutableArray<string>? disabledDiagnostics = null) : base(additionalAssemblies, references, disabledDiagnostics)
-            {
-            }
-
-            protected override IEnumerable<AnalyzerReference> GetSolutionAnalyzerReferences()
-            {
-                if (!_addedAnalyzers)
-                {
-                    _addedAnalyzers = true;
-                    return base.GetSolutionAnalyzerReferences();
-                }
-
-                return Enumerable.Empty<AnalyzerReference>();
-            }
-        }
-
-
     }
 }
